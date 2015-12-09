1a6fa8c6ec47505a774b435e0e3e96ff740dbc60
Emile Hamwey <ehamwey@team3467.org>
2015-12-09 08:09:00 -0500

    Update IterativeRobotCustom.java

--- src/org/usfirst/frc3467/IterativeRobotCustom.java
+++ src/org/usfirst/frc3467/IterativeRobotCustom.java
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) FIRST 2008-2012. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* must be accompanied by the FIRST Robotics BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 package org.usfirst.frc3467;
